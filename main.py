1a437a5c740601228a75afb9c4543bbac198780a
SullivanENPC <sullivan.castro@eleves.enpc.fr>
2022-11-16 09:36:25 +0100

    optimized version

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import numpy as np
 from numpy.random import normal
+from tqdm import tqdm
 import pickle
 import logging
 
 # global parameters
-logging.basicConfig(filename='montecarlog.log', encoding='utf-8', level=logging.DEBUG)
+logging.basicConfig(filename="montecarlog.log", encoding="utf-8", level=logging.DEBUG)
 S0 = 100  # price at time 0
 K = S0  # Strike
 R = 0.0001  # interest rate
 SIGMA = 0.05  # volatility
-T = 5  # Maturity
-T_rep = 10**9
+T = 100  # Maturity
+T_rep = 10**8
 X = np.linspace(0, T, T)  # abscisse
 Y = np.linspace(0, T_rep, T_rep)
 MU_STANDARD = 0  # mean
@@ -18,37 +19,55 @@ SIGMA_STANDARD = 1  # standard deviation
 STEP = 500
 
 
+@np.vectorize
 def W(t):
-    W_motion = [0]
-    for _ in range(1, t):
-        W_motion.append(normal(MU_STANDARD, SIGMA_STANDARD) + W_motion[-1])
-    return np.array(W_motion)
-
-
-def S(t):
-    s = [S0]
-    brown_motion = W(t)
+    W_motion = normal(MU_STANDARD, SIGMA_STANDARD, size=t)
+    W_motion[0] = 0
     for k in range(1, t):
-        s.append(s[-1] * (1 + R + SIGMA * (brown_motion[k] - brown_motion[k - 1])))
-    return np.array(s[-1])
+        W_motion[k] += W_motion[k - 1]
+    return W_motion
 
 
-s = [S(t) for t in range(T_rep)]
+@np.vectorize
+def S(t):
+    global pbar
+    brown_motion = W(t)
+    s = 1 + R + SIGMA * (brown_motion[1:] - brown_motion[:-1])
+    s = np.insert(s, 0, S0)
+    for k in range(1, t):
+        s[k] *= s[k - 1]
+    pbar.update(1)
+    return s[-1]
 
 
-put = lambda x: max(x - K, 0)
+with tqdm(total=T_rep) as pbar:
+    s = S(np.full((T_rep,), T))
 
 
-payoff_exp = list(map(put, s))
+@np.vectorize
+def put(x):
+    return max(x - K, 0)
 
 
-def Monte_Carlo(M, payoff_list):
-    return 1 / M * np.sum(payoff_list[:M])
+@np.vectorize
+def call(x):
+    return max(K - x, 0)
+
+
+payoff_exp = put(s)
+
+
+@np.vectorize
+def Monte_Carlo(M):
+    global pbar
+    pbar.update(1)
+    return np.mean(payoff_exp[:M])
+
 
 try:
     MC = list(map(Monte_Carlo, np.arange(1, T_rep, 1), [payoff_exp] * (T_rep - 1)))
 except:
-    logging.warning('Time exceeded')
+    logging.warning("Time exceeded")
 
 with open("monte_carlo_result.pkl", "wb") as f:
     pickle.dump(MC, f)
